ba8f2878ba9e700a01d78783fe50d25b529802c9
rei152 <andy.cheung@he-arc.ch>
2015-05-03 12:36:58 +0200

    fonction de calcule de précision + correction bug

--- Classificateur.py
+++ Classificateur.py
@@ -6,78 +6,55 @@ import random,math
 DIR_POSEV = './pos'
 DIR_NEGEV = './neg'
 
-# map mots -> occurence
+corpus = {}
 
-# def getCorpus(DIR,forbiddenWords):
-#
-#     corpusTrain,corpusTest = [],[]
-#
-#     LEN = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
-#
-#     for i in range(0,int(LEN*0.2)):
-#         while(True):
-#             n = random.randint(0,999)
-#             if(n not in corpusTest):
-#                 break
-#
-#         corpusTest.append(n)
-#
-#         for i in range (LEN):
-#             if(i not in corpusTest):
-#                 corpusTrain.append(i)
-#
-#     return corpusTest, corpusTrain
+corpusTestPos = {}
+corpusTestNeg = {}
 
-corpusTestPos = []
-corpusTestNeg = []
+corpusTrainPos = {}
+corpusTrainNeg = {}
 
-corpusTrainPos = []
-corpusTrainNeg = []
-
+totalMotsPos = 0
+totalMotsNeg = 0
 
 global DIR_NEGEV
 global DIR_POSEV
 
 
+# ------------------------------
 # map mots -> occurence
+# -----------------------------
 def loadFile_TAGGED(DIR):
     # print("--- lecture fichier ---")
 
     dico = {}
 
     # Open a file
-    # print(DIR)
     file = codecs.open(DIR, "r",'utf-8')
-    # print("Name of the file: ", file.name)
-
     line = file.read()
-    # print("Read Line: %s" % (line))
 
     poubelle = line.split('\n')
 
     for i in range(len(poubelle)-1):
-        # print("poubelle " + poubelle[i])
         try:
             mot = poubelle[i].split('\t')[2].rstrip('\r')
         except IndexError:
             mot = poubelle[i].rstrip('\r')
-
-        # print(mot)
 
         if(mot in dico):
             dico[mot] += 1
         else:
             dico[mot] = 1
 
-        # estimationLaplace(dico)
-
-
     # Close opend file
     file.close()
 
     return dico
 
-
+# ------------------------------
+# Récupère une liste de fichier dans un dossier
+# Calcule et séparer la liste entre le corpsus et les tests
+# -----------------------------
 def getFolderList_TAGGED(DIR):
     f = []
 
@@ -92,7 +69,7 @@ def getFolderList_TAGGED(DIR):
     random.shuffle(f)
 
     for n in range(int(len(f))):
-        if(n < int(len(f) * 0.2)):
+        if(n > int(len(f) * 0.2)):
             # corpusTrain = loadFile_TAGGED(mapWord, DIR+"/"+f[n])
             corpusTest = loadFile_TAGGED(DIR+"/"+f[n])
         else:
@@ -100,21 +77,17 @@ def getFolderList_TAGGED(DIR):
 
     return corpusTrain,corpusTest
 
+# ------------------------------
+# Charge les mots dans une map et compte leurs appararitions
+# -----------------------------
 def loadFIleToMap(DIR, mapToPeuple):
-    # mapToPeuple = {}
 
     # Open a file
     file = codecs.open(DIR, "r",'utf-8')
-    # print("Name of the file: ", file.name)
-
     line = file.read()
-    # print("Read Line: %s" % (line))
     poubelle = line.split('\n')
-    # print(len(poubelle))
-    # print(poubelle)
 
     for i in range(len(poubelle)):
-        # print("poubelle " + poubelle[i])
         try:
             mot = poubelle[i].split('\t')[2].rstrip('\r')
         except IndexError:
@@ -128,40 +101,53 @@ def loadFIleToMap(DIR, mapToPeuple):
     # Close opend file
     file.close()
 
-def estimationLaplace(estimationMap):
-    for i in estimationMap.keys():
-        estimationMap[i] += 1
+# ------------------------------
+# Enlève les mots d'une map à partir d'une liste
+# -----------------------------
+def removeForbidden(wordsMap, forbiddenList):
 
-def main():
+    tmpMap = wordsMap.copy()
+
+    for i in wordsMap:
+        if i in forbiddenList:
+            tmpMap.pop(i,None)
+
+    return tmpMap
+
+# ------------------------------
+# Fonction Bayes
+# -----------------------------
+def FromBayesWithLove(withForbidden = 0):
     forbidden = {}
 
-    loadFIleToMap("frenchST.txt",forbidden)
+    loadFIleToMap("frenchST.txt", forbidden)
+    forbiddenWords = forbidden.keys()
 
-    # corpusTestPos, corpusTrainPos = getCorpus(DIR_NEGEV,forbidden)
-    # corpusTestNeg, corpusTrainNeg = getCorpus(DIR_POSEV,forbidden)
+    corpusTrainNeg, corpusTestNeg = getFolderList_TAGGED("./tagged/neg")
+    corpusTrainPos, corpusTestPos = getFolderList_TAGGED("./tagged/pos")
 
-    corpusTrainNeg,corpusTestNeg = getFolderList_TAGGED("./tagged/neg")
-    corpusTrainPos,corpusTestPos = getFolderList_TAGGED("./tagged/pos")
-    # getFolderList_TAGGED(mapPosWords,"./tagged/pos")
+    if withForbidden == 0:
+        corpusTrainNeg = removeForbidden(corpusTrainNeg,forbiddenWords)
+        corpusTestNeg = removeForbidden(corpusTestNeg,forbiddenWords)
+        corpusTrainPos = removeForbidden(corpusTrainPos,forbiddenWords)
+        corpusTestPos = removeForbidden(corpusTestPos,forbiddenWords)
 
     # mapNegWords = corpusTrainNeg.update(corpusTestNeg)
     # mapPosWords = corpusTrainPos.update(corpusTestPos)
-
     # mapNegProba = {}
     # mapPosProba = {}
-
     # print("corpusTestNeg")
     # print(corpusTestNeg)
     # print("corpusTestPos")
     # print(corpusTestPos)
 
-    totalWords = len(corpusTrainNeg)+len(corpusTrainPos)
-    corpus = mergeMap(corpusTrainNeg,corpusTrainPos)
-    print(len(corpus))
-
+    # totalWords = len(corpusTrainNeg) + len(corpusTrainPos)
+    corpus = mergeMap(corpusTrainNeg, corpusTrainPos)
+    print("corpus len: ", len(corpus))
+    totalWords = len(corpus)
     # corpus = dict(corpusTrainNeg.items() + corpusTrainPos.items())
-    print("Corpsu %s",corpus)
 
+    print("Corpsu: ", corpus)
     # print(totalWords)
     # print("Negative words")
     # print(mapNegWords)
@@ -170,29 +156,54 @@ def main():
     # print("Positive words")
     # print(mapPosWords)
     # print(len(mapPosWords))
-
     print("proba neg")
-    mapNegProba = calculOccurance(corpusTrainNeg,totalWords)
-    print(corpusTrainNeg)
 
+    print("totalWords",totalWords)
+    mapNegProba = calculOccurance(corpusTrainNeg, totalWords)
+
+    print(len(corpusTrainNeg))
     print("proba pos")
-    mapPosProba = calculOccurance(corpusTrainPos,totalWords)
-    print(mapPosProba)
 
-    valusNeg = eval(mapNegProba,corpusTrainNeg)
-    valusPos = eval(mapPosProba,corpusTrainPos)
+    mapPosProba = calculOccurance(corpusTrainPos, totalWords)
+    print("mapPosProba",len(mapPosProba))
+
+    valusNeg = eval(mapNegProba, corpusTrainNeg)
+    valusPos = eval(mapPosProba, corpusTrainPos)
+
+    checkPrecision(corpusTrainPos,corpusTestPos,corpusTrainNeg,corpusTestNeg)
 
     if valusPos > valusNeg:
         print("Test positive, précision", valusPos)
     else:
         print("Test negative, précision", valusNeg)
 
+def checkPrecision(corpusTrainPos,corpusTestPos,corpusTrainNeg,corpusTestNeg):
+
+    countNeg = 0
+    countPos = 0
+
+    print(corpusTrainPos)
+
+    for i in corpusTestNeg:
+        if i in corpusTrainNeg and i not in corpusTrainPos:
+            countNeg+=1
+
+    for i in corpusTestPos:
+        if i in corpusTrainPos and i not in corpusTrainNeg:
+            countPos+=1
+
+    print("Mot positiove correctement classe",countPos/len(corpusTestPos) )
+    print("Mot negative correctement classe", countNeg/len(corpusTestNeg))
+
+# ------------------------------
+# Fusionne deux maps
+# -----------------------------
 def mergeMap(dicoA, dicoB):
 
     newDico = dicoA.copy()
 
     for i in dicoB.keys():
-        if i in newDico:
+        if i in dicoA.keys():
             newDico[i]+= dicoB[i]
 
         else:
@@ -200,59 +211,74 @@ def mergeMap(dicoA, dicoB):
 
     return newDico
 
+
+# ------------------------------
+# Fonction évaluation
+# -----------------------------
 def eval(dicoMyWord, corpusTrain):
     # dicoMyWord = {}
     print(corpusTrain)
     print(dicoMyWord)
 
     dicoEval = {}
-    valusEval = 1
+
+    valueTotal = 1
     # loadFIleToMap(FILE_DIR,dicoMyWord)
 
     for i in dicoMyWord.keys():
         if i in corpusTrain:
             dicoEval[i] = dicoMyWord[i]
 
-            # print(" -- insert dico -- ")
-            # print(i)
-            # print(dicoEval[i])
-
     for i in dicoEval.keys():
         # valueLog = math.log(math.pow(dicoEval[i],corpusTrain[i]))
         # valusEval+=dicoEval[i]
-        valusEval+=math.pow(dicoEval[i],corpusTrain[i])
 
-        print(" -- eval -- ")
-        print(i)
-        print(dicoEval[i])
+        valusEval= math.pow(dicoEval[i],corpusTrain[i])
+        valueLog = math.log(valusEval)
 
-        print(" -- corpus train -- ")
-        print(corpusTrain[i])
+        print(" ---------------- new ------------------- ")
+        print("mot: ", i)
+        print("dicoEval[i]",dicoEval[i])
+        print("corpusTrain[i]",corpusTrain[i])
+        print("valusEval",valusEval)
+        print("valueLog",valueLog)
+        print("valueTotal",valueTotal)
 
+        valueTotal+=valueLog
 
         print(" -- valuesEval -- ")
         print(valusEval)
 
         print()
 
-    valueLog = math.log(valusEval)
-    print(valueLog)
+    valueTotal += math.log(0.5)
+    # valueTotal *= 0.5
+
+    print("valueTotal ",valueTotal)
 
     # print(dicoEval)
     # print("final val : " + str(valusEval))
 
-    return valusEval
+    return valueTotal
 
-
+# ------------------------------
+# Permet de calculer les ocurances
+# -----------------------------
 def calculOccurance(mapWord,totalWord):
     newMap = {}
 
     for i in mapWord:
-        newMap[i] = mapWord[i]/totalWord
+        newMap[i] =( mapWord[i] + 1 )/ (len(mapWord) + totalWord)
+        print("len(mapWord)",len(mapWord))
+        print("newMap[i]", newMap[i])
+        print("totalWord",totalWord)
         # print(" --- occurance calcule --- ")
         # print(newMap[i])
 
     return newMap
+
+def main():
+    FromBayesWithLove()
 
 if __name__ == '__main__':
     main()
